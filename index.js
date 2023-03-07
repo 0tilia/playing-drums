bd970100271af5a2a94db0fe952e13fde50a8a3e
Otilia Toma <otilia@Otilias-MacBook-Air.local>
2023-03-07 23:06:59 +0200

    second commit

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ for (var i = 0; i < numberOfDrumButtons; i++) {
        var buttonInnerHTML = this.innerHTML;
 
        makeSound(buttonInnerHTML);
+       buttonAnimation(buttonInnerHTML)
 
     });
 
@@ -15,6 +16,9 @@ for (var i = 0; i < numberOfDrumButtons; i++) {
 document.addEventListener("keydown", function (event) {
 
     makeSound(event.key)
+    buttonAnimation(event.key)
+
+
 
 })
 
@@ -60,5 +64,18 @@ function makeSound(key) {
 
            default: console.log(buttonInnerHTML)
     }
+
+}
+
+function buttonAnimation(currentKey) {
+
+    var activeButton = document.querySelector("." + currentKey);
+    
+    activeButton.classList.add("pressed");
+    
+    setTimeout(function() {
+        activeButton.classList.remove("pressed");
+
+    }, 100);
 
 }
